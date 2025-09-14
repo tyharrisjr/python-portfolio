52ee5bb334e9549361b01ba6069b3624d0089324
TJ Harris <tj.harris118@gmail.com>
2025-09-13 22:18:47 -0400

    day 1

---
+++ one_hundred_days_of_python/Day 1/task.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name is: " + city + " " + pet)
